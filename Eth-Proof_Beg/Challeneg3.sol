87080f406fee0da614df0874ca4527cc6705a236
Moinak <111623431+Cyberninja30@users.noreply.github.com>
2024-06-02 22:32:38 +0530

    Update Challeneg3.sol

--- Eth-Proof_Beg/Challeneg3.sol
+++ Eth-Proof_Beg/Challeneg3.sol
@@ -1,1 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+/**
+ * @title MyToken
+ * @dev A simple ERC20-like token implementation with mint and burn functionality
+ */
+contract MySecondContract {
+    // Public variables to store token details
+    string public tokenName = "MANGO";
+    string public tokenAbbrv = "MNG";
+    uint public totalSupply = 0;
+
+    // Mapping from address to balance
+    mapping(address => uint) public balances;
+
+    // Event to log minting activity
+    event Mint(address indexed to, uint256 value);
+
+    // Event to log burning activity
+    event Burn(address indexed from, uint256 value);
+
+    // Function to mint new tokens
+    function mint(address _address, uint _value) public {
+        totalSupply += _value;
+        balances[_address] += _value;
+        emit Mint(_address, _value);
+    }
+
+    // Function to burn tokens
+    function burn(address _address, uint _value) public {
+        require(balances[_address] >= _value, "Insufficient balance to burn");
+        totalSupply -= _value;
+        balances[_address] -= _value;
+        emit Burn(_address, _value);
+    }
+}
